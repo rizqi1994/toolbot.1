fbfcd8f3e0f660a1ca17734a8f513bad93bc62a1
rizqi1994 <42020333+rizqi1994@users.noreply.github.com>
2018-10-01 14:56:10 +0700

    Add files via upload

---
+++ installv1.sh
@@ -0,0 +1,5 @@
+#memindahkan file bot ke home
+
+cd $HOME
+cd toolbot.1
+mv bot $HOME
---
+++ tool.sh
@@ -0,0 +1,13 @@
+#info
+#BY : Moh Bagus Romadhoni Pratama
+
+
+#unzip bot.zip
+
+
+cd $HOME
+cd toolbot.1
+echo " process... "
+unzip bot.zip
+sleep 1
+sh installv1.sh
